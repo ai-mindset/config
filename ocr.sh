c7979835087a399c27356eb262fcf9f47ed3d99e
Eirini <49712302+ai-mindset@users.noreply.github.com>
2025-01-01 15:37:18 +0000

    fix: OCR improvements

--- ocr.sh
+++ ocr.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-# Usage: ./ocr.sh full/path/to/image.jpg
+# Usage: ./ocr.sh image.jpg
 
 # From https://github.com/bytefer/ollama-ocr/blob/main/src/config.ts#L6C43-L11C68
 SYSTEM="Act as an OCR assistant. Analyze the provided image and:
@@ -11,4 +11,7 @@ SYSTEM="Act as an OCR assistant. Analyze the provided image and:
     Provide only the transcription without any additional comments." 
 
 echo "Using llama3.2-vision for OCR..."
-ollama run llama3.2-vision:latest $SYSTEM "$1" > ~/transcript_ocr.txt
+IMAGE_NAME=$(basename -- "$1") # Extract image name excluding the full path and extension
+CWD=$(pwd)
+
+ollama run llama3.2-vision:latest $SYSTEM "$CWD/$1" > "$CWD/$IMAGE_NAME.txt" 
